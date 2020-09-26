a7967b6e7617c08164c009b28c8b0ec90b3f5f29
PARAG <paragranerab1998@gmail.com>
2020-09-26 20:30:49 +0530

    Update 1

--- app/src/main/java/com/example/goodboxtask/MainActivity.java
+++ app/src/main/java/com/example/goodboxtask/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.goodboxtask;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.github.javiersantos.appupdater.AppUpdater;
+import com.github.javiersantos.appupdater.AppUpdaterUtils;
+import com.github.javiersantos.appupdater.enums.AppUpdaterError;
+import com.github.javiersantos.appupdater.enums.UpdateFrom;
+import com.github.javiersantos.appupdater.objects.Update;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		AppUpdaterUtils  appUpdaterUtils  = new AppUpdaterUtils(this)
+				.setUpdateFrom(UpdateFrom.GITHUB)
+				.setGitHubUserAndRepo("Parag171998", "Goodbox-Task")
+				.withListener(new AppUpdaterUtils.UpdateListener() {
+					@Override
+					public void onSuccess(Update update, Boolean isUpdateAvailable) {
+						Toast.makeText(MainActivity.this, Boolean.toString(isUpdateAvailable), Toast.LENGTH_SHORT).show();
+					}
+
+					@Override
+					public void onFailed(AppUpdaterError error) {
+						Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+					}
+				});
+		appUpdaterUtils .start();
+
 	}
 }
